4387de5385c95c2e6f8e8f380531c37bee2867d9
John Matthew David <itsmejomada@gmail.com>
2023-10-29 09:10:12 +0700

    table struct

---
+++ model/barang.go
@@ -0,0 +1,8 @@
+package model
+
+type Barang struct {
+	ID          int
+	Nama        string
+	Harga       int
+	Stok_Barang int
+}
---
+++ model/customer.go
@@ -0,0 +1,7 @@
+package model
+
+type Customer struct {
+	ID              int
+	Nama            string
+	Alamat_Customer string
+}
